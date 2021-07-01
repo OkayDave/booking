cb5a3ff2cea8b94bad23733262fe1800db0f95f2
Dave Russell <dave.kerr@gmail.com>
2021-07-01 23:44:45 +0100

    Add timeslot model and generation service

--- app/models/facility/base.rb
+++ app/models/facility/base.rb
@@ -1,14 +1,24 @@
 module Facility
   class Base < ApplicationRecord
     VALID_TYPES = %w[
-      ::Facility::Tennis
-      ::Facility::Volleyball
-      ::Facility::Football
-      ::Facility::Basketball
+      Facility::Tennis
+      Facility::Volleyball
+      Facility::Football
+      Facility::Basketball
+    ].freeze
+
+    # Standard opening times for the facility (10am - 7pm)
+    OPEN_HOURS = (10...20).freeze
+
+    # Dates when the facility is completely closed
+    EXCLUDED_DATES = [
+      '25/12'
     ].freeze
 
     validates :type, presence: true, inclusion: { in: VALID_TYPES }
     validates :name, presence: true, length: { minimum: 1, maximum: 255 }
+
+    has_many :timeslots, inverse_of: :facility, foreign_key: :facility_id, class_name: 'Timeslot'
 
     def serializable_hash(opts = {})
       opts = {
@@ -16,6 +26,10 @@ module Facility
       }.update(opts)
 
       super(opts)
+    end
+
+    def excluded_dates
+      EXCLUDED_DATES
     end
   end
 end
